b5d123d3bee181d46d8a7c14367297568b6dd271
totu <topituulensuu@fastmail.com>
2020-02-19 18:08:43 +0200

    add /matrix

---
+++ assets/matrix.js
@@ -0,0 +1,89 @@
+let matrix = document.getElementById("matrix");
+var anime_count = 0;
+var columns = [];
+
+(function() {
+    matrix.innerHTML = "";
+})();
+
+let rand_alpha = function(count, old='') {
+    var rand = old + Math.random().toString(36).substring(2);
+    if (rand.length < count) {
+        rand = rand_alpha(count, rand);
+    }
+    return rand.substring(rand.length-count);
+}
+
+let string = function(len) {
+    rand = rand_alpha(len);
+    str = "";
+    for (var i=0; i<rand.length; i++) {
+        str += "<span class='matrix-chr'>" + rand[i] + '</span>';
+    }
+    return str;
+}
+
+let sleep = function(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let randomize_column = function() {
+    var random_column = Math.floor(Math.random() * 55 + 1);
+    do {
+        random_column = Math.floor(Math.random() * 55 + 1);
+    }
+    while (columns.indexOf(random_column) != -1);
+    columns.push(random_column);
+    return random_column;
+}
+
+let animate = async function() {
+    anime_count += 1;
+    let random_column = randomize_column();
+    let speed = Math.floor(Math.random() * 55 + 50);
+    let random_len = Math.floor(Math.random() * matrix_size/2/2 + 1);
+    let rows = document.getElementsByClassName("matrix-row");
+
+    for (var i=0; i<rows.length+random_len+1; i++) {
+        for (var j=0; j<i-random_len; j++) {
+            let row = rows[j];
+            let child = row.children[random_column];
+            child.classList.remove('matrix-after');
+        }
+        for (var j=i-random_len; j<i; j++) {
+            let row = rows[j];
+            if (row != undefined) {
+                let child = row.children[random_column];
+                child.classList.add('matrix-after');
+                child.classList.remove('matrix-green');
+            }
+        }
+        let row = rows[i];
+        if (row != undefined) {
+            let child = row.children[random_column];
+            if (child != undefined) {
+                child.classList.add('matrix-green');
+            }
+        }
+        await sleep(speed);
+    }
+    columns.splice(columns.indexOf(random_column), 1);
+    anime_count -= 1;
+}
+
+window.addEventListener('load', async function() {
+    matrix_size = 55;
+    str = ""
+    for (var i=0; i<matrix_size/2; i++) {
+        str += "<div class='matrix-row matrix-row-"+i+"'>" + string(matrix_size) + "</div>";
+    }
+    matrix.innerHTML = str;
+    while(true) {
+        if (anime_count < 10) {
+            animate();
+        }
+        await sleep(250);
+    }
+})
+
+
