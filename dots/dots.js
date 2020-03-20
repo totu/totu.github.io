f61fb5fd36b49eb98e45486c2a01f4a42c37845c
totu <topituulensuu@fastmail.com>
2020-03-20 21:23:28 +0200

    more dots

--- dots/dots.js
+++ dots/dots.js
@@ -1,15 +1,11 @@
 let main = document.getElementById("main");
 
-(function() {
-    main.innerHTML = "";
-})();
-
-let get = function(title, url, cb, syntax) {
+let get = function(title, url, cb, comment, syntax) {
     var ajax = new XMLHttpRequest();
     ajax.open("GET", url, true);
     ajax.onreadystatechange = function () {
         if (ajax.readyState != 4 || ajax.status != 200) return undefined;
-        cb(ajax.responseText, syntax, title);
+        cb(ajax.responseText, syntax, comment, title);
     };
     ajax.send();
 }
@@ -32,7 +28,7 @@ let highlight = function(txt, syntax) {
     return txt
 }
 
-let print = function(txt, syntax, title) {
+let print = function(txt, syntax, comment, title) {
     html = "<h1>" + title + "</h1>";
     last_was_comment = false;    // this keeps track of "code blocks" and comments
 
@@ -42,12 +38,16 @@ let print = function(txt, syntax, title) {
         let row = sanitize(txt[i])
 
         // Handle comments
-        if (row.startsWith('&quot;')) {
+        if (row.startsWith(comment)) {
             // If last row was code, close "code block" first
             if (!last_was_comment) {
                 html += "</div>";
             }
-            html += "<p class='dot-comment'>" + row.substring(6).trim() + "</p>";
+            // Remove ending ] if git style heading
+            if (row.endsWith("]")) {
+                row = row.substring(0, row.length-1)
+            }
+            html += "<p class='dot-comment'>" + row.substring(comment.length).trim() + "</p>";
             last_was_comment = true;
 
         // Handle code lines
@@ -83,7 +83,7 @@ let add_tag = function(tag, keywords, flag="i") {
 let vim_syntag_generator = function() {
     let vim_keywords = ["let ", " set ", "^set ", "call ", "if ", "endif", "abbrev ", "au ", "colorscheme ", "inoremap", "tnoremap", "vnoremap ", "nnoremap ", "noremap ", "function! ", "endfunction", "autocmd ", "inoremap ", "command! ", "execute ", "vmap ", "nmap ", "cmap ", "map "];
     let vim_plugin_words = [",", "= ", "Plugin ", "filetype ", "^syntax ", ":"]
-    let vim_keybinds = ["&lt;.+?&gt;"];
+    let vim_keybinds = ["&#039.+?&#039", "&lt;.+?&gt;"];
     var vim_syntax = [["cyan", "&quot; .*", "i"]];
     vim_syntax = vim_syntax.concat(add_tag("yellow", vim_plugin_words))
     vim_syntax = vim_syntax.concat(add_tag("purple", vim_keybinds, "g"))
@@ -91,7 +91,30 @@ let vim_syntag_generator = function() {
     return vim_syntax;
 }
 
+let tmux_syntag_generator = function() {
+    let tmux_keywords = ["run-shell", "set-window-option", "^set-option", "setw", "^set"];
+    let tmux_plugin_words = ["&#039.+?&#039", " on", " off", "unbind-key", "bind-key", "unbind", "bind"]
+    let tmux_keybinds = [" -[a-zA-Z]+", "&quot;.*&quot;", "&lt;.+?&gt;"];
+    var tmux_syntax = [["cyan", "# .*", "i"]];
+    tmux_syntax = tmux_syntax.concat(add_tag("yellow", tmux_plugin_words))
+    tmux_syntax = tmux_syntax.concat(add_tag("purple", tmux_keybinds, "g"))
+    tmux_syntax = tmux_syntax.concat(add_tag("red", tmux_keywords))
+    return tmux_syntax;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    let vim_syntax = vim_syntag_generator();
-    get(".vimrc", "https://topituulensuu.com/dotfiles/vimrc", print, vim_syntax);
+    let url = window.location.toString().split("/");
+    let file = url[url.length-2];
+    let dot_files = {
+        ".vimrc": {"url": "https://topituulensuu.com/dotfiles/vimrc", "syntax": vim_syntag_generator(), "comment": "&quot;"},
+        ".tmux.conf": {"url": "https://topituulensuu.com/dotfiles/tmux.conf", "syntax": tmux_syntag_generator(), "comment": "#"},
+        ".gitconfig": {"url": "https://topituulensuu.com/dotfiles/gitconfig", "syntax": vim_syntag_generator(), "comment": "["}
+    }
+    console.log(file);
+    if (file in dot_files) {
+        url = dot_files[file]["url"]
+        syntax = dot_files[file]["syntax"]
+        comment = dot_files[file]["comment"]
+        get(file, url, print, comment, syntax);
+    }
 });
