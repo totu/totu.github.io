4b57d5b4bc13036c910b9c89ac8fea543784fc44
totu <topituulensuu@fastmail.com>
2020-03-29 09:44:50 +0300

    reorg

---
+++ assets/corona.js
@@ -0,0 +1,124 @@
+const main = document.getElementById("main");
+(function() {
+    main.innerHTML = "loading...";
+})();
+const url = "https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData"
+let db = {};
+
+const get = function(url, cb) {
+    const ajax = new XMLHttpRequest();
+    ajax.open("GET", url, true);
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState != 4 || ajax.status != 200) return undefined;
+        cb(ajax.responseText);
+    };
+    ajax.send();
+}
+
+const get_date = function(str) {
+    const date = new Date(str);
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    if (month < 10) month = "0" + month;
+    let day = date.getDate();
+    if (day < 10) day = "0" + day;
+    return year + "-" + month + "-" + day;
+}
+
+const order_dict = function(db, data) {
+    for (item in data) {
+        for (let i=0; i<data[item].length; i++) {
+            const date = get_date(data[item][i].date);
+            if (db[date] == undefined) {
+                db[date] = {
+                    "confirmed": 0,
+                    "deaths": 0,
+                    "recovered": 0
+                }
+            }
+        }
+    }
+    const ordered = {};
+    Object.keys(db).sort().forEach(function(key) {
+        ordered[key] = db[key];
+    });
+    return ordered;
+}
+
+const get_bar = function(count, max) {
+    const mark = "▉";
+    let bar = "";
+    const percent = Math.ceil(count/max*15);
+    for (let i=0; i<percent; i++) {
+        bar += mark;
+    }
+    return bar;
+}
+
+const draw_chart = function(data) {
+    data = JSON.parse(data);
+    db = order_dict(db, data);
+    con_total = 0;
+    ded_total = 0;
+    rec_total = 0;
+    for (item in db) {
+        db[item].confirmed += con_total;
+        for (con in data.confirmed) {
+            if (get_date(data.confirmed[con].date) == item) {
+                db[item].confirmed += 1;
+                con_total += 1;
+            }
+        }
+
+        db[item].deaths += ded_total;
+        for (ded in data.deaths) {
+            if (get_date(data.deaths[ded].date) == item) {
+                db[item].deaths += 1;
+                ded_total += 1;
+            }
+        }
+
+        db[item].recovered += rec_total;
+        for (rec in data.recovered) {
+            if (get_date(data.recovered[rec].date) == item) {
+                db[item].recovered += 1;
+                rec_total += 1;
+            }
+        }
+    }
+    const longest = con_total + ded_total + rec_total;
+
+    let html = "";
+    for (item in db) {
+        const confirmed = get_bar(db[item].confirmed, longest)
+        const deaths = get_bar(db[item].deaths, longest)
+        const recovered = get_bar(db[item].recovered, longest)
+        let date = item.split("-");
+        date = date[2] + "/" + date[1];
+        html = "<p>" + date + " "
+        + "<span class='confirmed box'>" + confirmed + "</span>"
+        + "<span class='deaths box'>" + deaths + "</span>"
+        + "<span class='recovered box'>" + recovered + "</span>"
+        + " "
+        + "<span class='count'>"
+        + "<span class='confirmed'>" + db[item].confirmed + "</span> "
+        + "/ "
+        + "<span class='deaths'>" + db[item].deaths + "</span> "
+        + "/ "
+        + "<span class='recovered'>" + db[item].recovered + "</span> "
+        + "</span>"
+        + "</p>" + html;
+    }
+
+    main.innerHTML = "<p>Covid-19 in Finland<span class='count'>"
+        + "<span class='confirmed'>confirmed</span> "
+        + "/ "
+        + "<span class='deaths'>deaths</span> "
+        + "/ "
+        + "<span class='recovered'>recovered</span> "
+        + "</p>" + html;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    get(url, draw_chart);
+});
--- assets/matrix.js
+++ assets/matrix.js
@@ -1,67 +1,69 @@
-let matrix = document.getElementById("matrix");
-let height = 55;
-var anime_count = 0;
-var columns = [];
+const matrix = document.getElementById("matrix");
+const height = 75;
+let anime_count = 0;
+let columns = [];
 
 (function() {
-    matrix.innerHTML = "";
+    matrix.innerHTML = "loading...";
 })();
 
-let rand_alpha = function(count, old='') {
-    var rand = old + Math.random().toString(36).substring(2);
+const rand_alpha = function(count, old='') {
+    let rand = old + Math.random().toString(36).substring(2);
     if (rand.length < count) {
         rand = rand_alpha(count, rand);
     }
     return rand.substring(rand.length-count);
 }
 
-let string = function(len) {
+const string = function(len) {
     rand = rand_alpha(len);
     str = "";
-    for (var i=0; i<rand.length; i++) {
+    for (let i=0; i<rand.length; i++) {
         str += "<span class='matrix-chr'>" + rand[i] + '</span>';
     }
     return str;
 }
 
-let sleep = function(ms) {
+const sleep = function(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let randomize_column = function() {
-    var random_column = Math.floor(Math.random() * height + 1);
+const randomize_column = function() {
+    let random_column = Math.floor(Math.random() * height + 1);
     do {
         random_column = Math.floor(Math.random() * height + 1);
     }
     while (columns.indexOf(random_column) != -1);
-    columns.push(random_column);
+    // columns.push(random_column);
     return random_column;
 }
 
-let animate = async function() {
+const animate = async function() {
     anime_count += 1;
-    let random_column = randomize_column();
-    let speed = Math.floor(Math.random() * height + 50);
-    let random_len = Math.floor(Math.random() * matrix_size/2/3 + 1);
-    let rows = document.getElementsByClassName("matrix-row");
+    const random_column = randomize_column();
+    const speed = Math.floor(Math.random() * height + 30);
+    const random_len = Math.floor(Math.random() * matrix_size/2/3 + 1);
+    const rows = document.getElementsByClassName("matrix-row");
 
-    for (var i=0; i<rows.length+random_len+1; i++) {
-        for (var j=0; j<i-random_len; j++) {
-            let row = rows[j];
-            let child = row.children[random_column];
+    for (let i=0; i<rows.length+random_len+1; i++) {
+        for (let j=0; j<i-random_len; j++) {
+            const row = rows[j];
+            const child = row.children[random_column];
             child.classList.remove('matrix-after');
         }
-        for (var j=i-random_len; j<i; j++) {
-            let row = rows[j];
+        for (let j=i-random_len; j<i; j++) {
+            const row = rows[j];
             if (row != undefined) {
                 let child = row.children[random_column];
-                child.classList.add('matrix-after');
-                child.classList.remove('matrix-green');
+                if (child != undefined) {
+                    child.classList.add('matrix-after');
+                    child.classList.remove('matrix-green');
+                }
             }
         }
         let row = rows[i];
         if (row != undefined) {
-            let child = row.children[random_column];
+            const child = row.children[random_column];
             if (child != undefined) {
                 child.classList.add('matrix-green');
             }
@@ -69,10 +71,10 @@ let animate = async function() {
         await sleep(speed);
     }
     await sleep(speed);
-    let new_col = rand_alpha(height);
-    for (var i=0; i<rows.length; i++) {
-        let row = rows[i];
-        let child = row.children[random_column];
+    const new_col = rand_alpha(height);
+    for (let i=0; i<rows.length; i++) {
+        const row = rows[i];
+        const child = row.children[random_column];
         child.innerHTML = new_col[i];
     }
     columns.splice(columns.indexOf(random_column), 1);
@@ -82,15 +84,15 @@ let animate = async function() {
 window.addEventListener('load', async function() {
     matrix_size = height;
     str = ""
-    for (var i=0; i<matrix_size/2; i++) {
+    for (let i=0; i<matrix_size/2; i++) {
         str += "<div class='matrix-row matrix-row-"+i+"'>" + string(matrix_size) + "</div>";
     }
     matrix.innerHTML = str;
     while(true) {
-        if (anime_count < 10) {
+        if (anime_count < 40) {
             animate();
         }
-        await sleep(250);
+        await sleep(Math.floor(Math.random() * 250));
     }
 })
 
