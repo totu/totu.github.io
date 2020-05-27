96e213250ade6ccce61f13fb73533e1644dc7303
totu <topituulensuu@fastmail.com>
2020-05-27 21:55:24 +0300

    portscan: as good as it gets

--- site/assets/scan.js
+++ site/assets/scan.js
@@ -3,24 +3,26 @@ const main = document.querySelector("#main");
 const results = {}
 let normalSort = true;
 let lastSort = 0;
-let ip = "localhost";
+let ip = "127.0.0.1";
+let last = 0
 
-const reportPort = async (type, socket) => {
-    const delta = performance.now() - socket.start;
+const reportPort = (type, socket) => {
+    let delta = (performance.now() - socket.start);
+    const tmp = last;
+    last = delta;
+    delta = delta - tmp;
     const scanPort = socket.port;
     results[scanPort][type] = delta + "ms";
     main.innerHTML = "";
     let html = "<table>";
     html += "<tr>";
     html += "<th>port</th>";
-    html += "<th>protocol</th>";
     html += "<th>time</th>";
     html += "</tr>";
     for (port in results) {
         for (type in results[port]) {
             html += "<tr>";
             html += "<td>" + port + "</td>";
-            html += "<td>" + type + "</td>";
             html += "<td>" + results[port][type] + "</td>";
             html += "</tr>";
         }
@@ -34,7 +36,7 @@ const reportPort = async (type, socket) => {
     });
 }
 
-const sortTableBy = async (column) => {
+const sortTableBy = (column) => {
     let table = document.querySelector("table");
     const content = []
     for (row in table.rows) {
@@ -61,13 +63,11 @@ const sortTableBy = async (column) => {
     }
     lastSort = sortingIndex;
     let html = "<tr><th>" + header[0] + "</th>";
-    html += "<th>" + header[1] + "</th>";
-    html += "<th>" + header[2] + "</th></tr>";
+    html += "<th>" + header[1] + "</th></tr>";
     for (let i=0; i<content.length; i++) {
         html += "<tr>";
         html += "<td>" + content[i][0] + "</td>";
         html += "<td>" + content[i][1] + "</td>";
-        html += "<td>" + content[i][2] + "</td>";
         html += "</tr>";
     }
     table.innerHTML = html;
@@ -78,62 +78,40 @@ const sortTableBy = async (column) => {
     });
 }
 
-const websocket = async (port) => {
+const websocket = (port) => {
     try {
         const socket = new WebSocket("ws://" + ip + ":" + port);
         socket.port = port;
         socket.start = performance.now();
-        socket.onerror = () => { reportPort("websocket", socket); }
-        socket.onopen = () => { reportPort("websocket", socket); }
+        socket.onerror = () => {
+            reportPort("websocket", socket);
+        }
+        socket.onopen = () => {
+            reportPort("websocket", socket);
+            socket.close();
+        }
     } catch(e) {
         console.log(ip, port)
         console.log("error:", e);
     }
 }
 
-const ajax = async (port) => {
-    const request = new XMLHttpRequest();
-    request.port = port;
-    request.start = performance.now()
-    request.onreadystatechange = () => {
-        if (request.readyState == 4) {
-            reportPort("http", request);
-        }
-    };
-    request.open("GET", ip + ":" + port, true);
-    request.send();
-}
-
-const img = async (port) => {
-    const image = new Image();
-    image.port = port;
-    image.start = performance.now();
-    image.onloadend = () => { reportPort("img", image); }
-    image.src = ip + ":" + port + "/image.jpg";
-}
-
-const scanner = async () => {
+const scanner = () => {
     for (port in results) {
-        // Reset times
-        const list = ["websocket", "http", "img"];
-        for (i in list) {
-            results[port][list[i]] = "--";
-        }
-        // Scan ports
         websocket(port);
-        ajax(port);
-        img(port);
     }
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    scan.addEventListener("click", async () => {
+document.addEventListener("DOMContentLoaded", () => {
+    scan.addEventListener("click", () => {
         const ports = document.querySelector("#ports").value.split(",");
-        ip = document.querySelector("#ip").value;
         for (i in ports) {
             const port = parseInt(ports[i]);
             if (typeof(port) == "number" && !(port in results))
                 results[port] = {}
+        }
+        for (port in results) {
+            results[port]["websocket"] = "--";
         }
         scanner();
     });
