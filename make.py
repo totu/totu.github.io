bdedb6c57949945281ee1e294d4a0e1e58b0d207
Topi 'top1' Tuulensuu <topi@topituulensuu.com>
2023-03-05 22:23:47 +0200

    teach make to also watch

--- make.py
+++ make.py
@@ -1,91 +1,143 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 from pathlib import Path
 import shutil
 import datetime
 import markdown
 
-content = Path("content")
-output = Path("output")
-templates = Path("templates")
-base_title = "»»t0p1 // "
-header = footer = str()
 
-# Clean output
-if output.exists():
-    shutil.rmtree(output)
-output.mkdir()
+class Watcher:
+    def __init__(self, content, generator):
+        self.observer = Observer()
+        self.content = content
+        self.generator = generator
 
-# Read templates
-with open(Path(templates / "header.html"), "r") as input_file:
-    header = input_file.read()
+    def run(self):
+        event_handler = Handler(generator)
+        self.observer.schedule(event_handler, self.content.name, recursive = True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Observer Stopped")
 
-with open(Path(templates / "footer.html"), "r") as input_file:
-    footer = input_file.read()
-
-# Handle content
-archives: list[tuple[Path, str, datetime.datetime]] = list()
-
-for thing in content.iterdir():
-    # Copy dirs directly to output
-    if thing.is_dir():
-        shutil.copytree(thing, Path(output / thing.name), copy_function=shutil.copy, dirs_exist_ok=True)
-
-    # Files are handles one-by-one
-    else:
-        # Prep file
-        file = header
-
-        # Get base name of the file
-        base_name = thing.name.split(".")[0]
-
-        # Handle HTML files
-        if thing.suffix == ".html":
-            file_title = base_title + base_name
-            file += f"<title>{file_title}</title>"
-            file += thing.read_text()
-            out_file = Path(output / thing.name)
-
-        # Handle Markdown files
-        elif thing.suffix == ".md":
-            with open(thing, "r") as input_file:
-                title = input_file.readline().split("title:")[1].strip()
-                date = input_file.readline().split("date:")[1].strip()
-                file_title = base_title + title
-                file += f"<title>{file_title}</title>"
-                file += markdown.markdown(input_file.read(), extensions=["fenced_code"])
-
-            # Construct proper file name
-            out_file = Path(output / f"{base_name}.html")
-            date_time = datetime.datetime.fromisoformat(date)
-            archives.append((out_file, title, date_time))
+        self.observer.join()
 
 
-        # Else throw a fit
-        else:
-            raise Exception("Unknown filetype {thing}")
+class Handler(FileSystemEventHandler):
+    def __init__(self, generator):
+        self.generator = generator
 
-        # Add footer
-        file += footer
+    def on_any_event(self, event):
 
-        # Write the file
-        out_file.write_text(file)
+        if event.is_directory:
+            return None
+
+        elif event.event_type in ["created", "modified", "deleted"]:
+            self.generator.clean()
+            self.generator.read_templates()
+            self.generator.generate_content()
 
 
-# Prep archive index
-archive_index = header + f"<title>{base_title} archives</title><ul class='archive'>"
+class Generator():
+    def __init__(self, content, output, templates, title):
+        self.content = content
+        self.output = output
+        self.templates = templates
+        self.title = title
+        self.header = str()
+        self.footer = str()
 
-# Sort archives based on date
-archives = sorted(archives, key=lambda x: x[2], reverse=True)
+    def clean(self):
+        if self.output.exists():
+            shutil.rmtree(self.output)
+        self.output.mkdir()
 
-# Create actual index
-for entry in archives:
-    entry_file, entry_title, entry_date = entry
-    # Convert date to "Jun 2021" format
-    entry_date = entry_date.strftime("%b %Y")
-    archive_index += f"<li><a href='/{entry_file.name}'>{entry_title}<span style='float:right'>{entry_date}</span></a></li>"
+    def read_templates(self):
+        with open(Path(self.templates / "header.html"), "r") as header:
+            self.header = header.read()
 
-# Close list and add footer
-archive_index += "</ul>" + footer
+        with open(Path(self.templates / "footer.html"), "r") as footer:
+            self.footer = footer.read()
 
-# Write archive index to file
-Path(output / "archives.html").write_text(archive_index)
+    def generate_content(self):
+        archives: list[tuple[Path, str, datetime.datetime]] = list()
+
+        for thing in self.content.iterdir():
+            # Copy dirs directly to output
+            if thing.is_dir():
+                shutil.copytree(thing, Path(self.output / thing.name), copy_function=shutil.copy, dirs_exist_ok=True)
+
+            # Files are handles one-by-one
+            else:
+                # Prep file
+                file = self.header
+
+                # Get base name of the file
+                base_name = thing.name.split(".")[0]
+
+                # Handle HTML files
+                if thing.suffix == ".html":
+                    file_title = self.title + base_name
+                    file += f"<title>{file_title}</title>"
+                    file += thing.read_text()
+                    out_file = Path(self.output / thing.name)
+
+                # Handle Markdown files
+                elif thing.suffix == ".md":
+                    with open(thing, "r") as markdown_file:
+                        title = markdown_file.readline().split("title:")[1].strip()
+                        date = markdown_file.readline().split("date:")[1].strip()
+                        file_title = base_title + title
+                        file += f"<title>{file_title}</title>"
+                        file += markdown.markdown(markdown_file.read(), extensions=["fenced_code"])
+
+                        # Construct proper file name
+                        out_file = Path(self.output / f"{base_name}.html")
+                        date_time = datetime.datetime.fromisoformat(date)
+                        archives.append((out_file, title, date_time))
+
+
+                # Else throw a fit
+                else:
+                    raise Exception("Unknown filetype {thing}")
+
+                # Add footer
+                file += self.footer
+
+                # Write the file
+                out_file.write_text(file)
+
+
+        # Prep archive index
+        archive_index = self.header + f"<title>{base_title} archives</title><ul class='archive'>"
+
+        # Sort archives based on date
+        archives = sorted(archives, key=lambda x: x[2], reverse=True)
+
+        # Create actual index
+        for entry in archives:
+            entry_file, entry_title, entry_date = entry
+            # Convert date to "Jun 2021" format
+            entry_date = entry_date.strftime("%b %Y")
+            archive_index += f"<li><a href='/{entry_file.name}'>{entry_title}<span style='float:right'>{entry_date}</span></a></li>"
+
+        # Close list and add footer
+        archive_index += "</ul>" + self.footer
+
+        # Write archive index to file
+        Path(self.output / "archives.html").write_text(archive_index)
+
+if __name__ == "__main__":
+    content = Path("content")
+    output = Path("output")
+    templates = Path("templates")
+    base_title = "»»t0p1 // "
+    generator = Generator(content=content, output=output, templates=templates, title=base_title)
+    watch = Watcher(content, generator)
+    watch.run()
+
--- watch.sh
+++
@@ -1,1 +0,0 @@
-inotifywait --recursive --monitor --format "%e %w%f" \\n--event modify,move,create,delete content | while read changed; do ./make.py; done
